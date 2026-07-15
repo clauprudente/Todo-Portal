41ac4cd08d09fba180d35e0389f95458e273438f
clauprudente <claudiamary.santos@gmail.com>
2026-07-15 17:44:15 -0300

    feat: implement local storage service

---
+++ src/services/todo.storage.ts
@@ -0,0 +1,22 @@
+import type { Todo } from '@/types/todo';
+
+const LOCAL_STORAGE_KEY = 'todo-list'
+
+export const getTodos = (): Todo[] => {
+    const data = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+    if (data) {
+        return JSON.parse(data);
+    } else {
+        return [];
+    }
+}
+
+export const saveTodos = (todos: Todo[]): boolean => {
+    try {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+        return true;
+    } catch {
+        return false;
+    }
+};
---
+++ src/types/todo.ts
@@ -0,0 +1,8 @@
+export type Todo = {
+    id: string;
+    title: string;
+    description?: string;
+    created_at: number;
+    completed: boolean;
+}
+
